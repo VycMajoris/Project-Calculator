7423f9e2175cc7c84653d830a5c41d825c8b236e
VycMajoris <ok1907@gmail.com>
2022-12-28 01:33:22 +0300

    continue to work on storing values and using them with right operator functions.

--- first.js
+++ first.js
@@ -1,11 +1,11 @@
 let outmostContainer = document.getElementById('outmostContainer');
 // text for buttons 
 let buttonText = ["AC", "+/-", "%", "/",
-                 "7", "8", "9" ,"X", "4",
+                 "7", "8", "9" ,"x", "4",
                 "5", "6", "-", "1","2",
                 "3", "+", "0", ",", "=",];
 
-let operatorsArr = ["C", "+/-", "%", "/", "x", "-", "+", "=", ",",];
+let operatorsArr = ["+/-", "%", "/", "x", "-", "+",];
 
 // create buttons for calculator
 for (let i=1; i<=19; i++) {
@@ -40,6 +40,14 @@ function multiply (num1,num2) {
 function divide (num1,num2) {
     return num1/num2;
 }
+
+function percent (num) {
+    return num/100;
+}
+
+function changeSign (num) {
+    return -num;
+}
 // deciding which function to use depending on the operator type
 function operate (operator, num1, num2) {
     if(operator === '+') add(num1,num2)
@@ -55,15 +63,31 @@ function operate (operator, num1, num2) {
 // select screenDiv
 let screenDiv = document.getElementById('screenDiv');
 
+let currentCalcResult = 0;
 
  function displayFunc (e) {
 
     // TODO: If the pressed button is a number, add the button to whatever is on screen.
     //       Else, use the selected operator to calculate.
+        let leftNum = 0;
+        let rightNum = 0;
         let convertedNumber = Number(e.target.name);
-        
-        if(convertedNumber) && screenDiv.textContent) 
-        screenDiv.textContent = e.target.name;
+        // check if the pressed button is a number
+        if(Number.isFinite(convertedNumber)) {
+            screenDiv.textContent += convertedNumber;
+            
+            
+        } else if(operatorsArr.includes(e.target.name)) {
+            leftNum = Number(screenDiv.textContent);
+            //screenDiv.textContent = '';
+            screenDiv.textContent = leftNum+ ' ' + e.target.name + ' ';
+            // console.log(screenDiv.textContent);
+            rightNum = Number(screenDiv.textContent.substring((screenDiv.textContent.indexOf('+')) + 2));
+            console.log(leftNum, rightNum);
+            screenDiv.textContent = add(leftNum, rightNum); 
+            
+        }
+       
     
     
 }
