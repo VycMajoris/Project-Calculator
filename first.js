12d63abd01cfa04ab5e1cedf6dd981569ff2320b
VycMajoris <ok1907@gmail.com>
2022-12-28 00:32:56 +0300

    change how event listeners are implemented and start evaluating what kind of character's button is pressed.

--- first.js
+++ first.js
@@ -1,20 +1,25 @@
 let outmostContainer = document.getElementById('outmostContainer');
-
+// text for buttons 
 let buttonText = ["AC", "+/-", "%", "/",
                  "7", "8", "9" ,"X", "4",
                 "5", "6", "-", "1","2",
                 "3", "+", "0", ",", "=",];
 
+let operatorsArr = ["C", "+/-", "%", "/", "x", "-", "+", "=", ",",];
 
+// create buttons for calculator
 for (let i=1; i<=19; i++) {
     let createBtn = document.createElement('button');
+    // name every button same as its text content
+    createBtn.name = buttonText[i-1];
     createBtn.id = 'buttons';
     createBtn.textContent= buttonText[i-1];
     createBtn.style.width= '65px';
     createBtn.style.height = '65px';
     outmostContainer.appendChild(createBtn);
     createBtn.style.backgroundColor = 'yellow';
-
+    // add event listeners for each button
+    createBtn.addEventListener ('click', displayFunc);
      if (createBtn.textContent === '0') {
         createBtn.style.width = '130px';
     }     
@@ -35,7 +40,7 @@ function multiply (num1,num2) {
 function divide (num1,num2) {
     return num1/num2;
 }
-
+// deciding which function to use depending on the operator type
 function operate (operator, num1, num2) {
     if(operator === '+') add(num1,num2)
     if(operator === '-') add(num1,num2)
@@ -43,8 +48,25 @@ function operate (operator, num1, num2) {
     if(operator === '/') add(num1,num2)
 
 }
+ 
 
 
+
+// select screenDiv
+let screenDiv = document.getElementById('screenDiv');
+
+
+ function displayFunc (e) {
+
+    // TODO: If the pressed button is a number, add the button to whatever is on screen.
+    //       Else, use the selected operator to calculate.
+        let convertedNumber = Number(e.target.name);
+        
+        if(convertedNumber) && screenDiv.textContent) 
+        screenDiv.textContent = e.target.name;
+    
+    
+}
 
 
 
