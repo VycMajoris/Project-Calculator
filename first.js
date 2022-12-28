50c8b1e57c188b7cf78edad1767846ec3583edbb
VycMajoris <ok1907@gmail.com>
2022-12-28 23:43:23 +0300

    add logic to the calculator but result is unstable when the operator changed on the fly.

--- first.js
+++ first.js
@@ -1,22 +1,23 @@
 let outmostContainer = document.getElementById('outmostContainer');
+
 // text for buttons 
 let buttonText = ["AC", "+/-", "%", "/",
                  "7", "8", "9" ,"x", "4",
                 "5", "6", "-", "1","2",
                 "3", "+", "0", ",", "=",];
 
- let buttonValue = ["AC", "+/-", "%", "/",
-                7, 8, 9 ,"x", 4,
-               5, 6, "-", 1 , 2,
-               3, "+", "0", ",", "=",];                
+ let numButtonsArr = ["0", "1", "2", "3" , "4",
+                    "5", "6", "7", "8" , "9"];                
 
 let operatorsArr = ["+/-", "%", "/", "x", "-", "+", "="];
 
 let operators = "+/-%x";
 
-// create buttons for calculator
+// create buttons for numbers
 for (let i=1; i<=19; i++) {
     let createBtn = document.createElement('button');
+    let createNumBtn = document.createElement('button');
+    let clearButton = document.createElement('button');
     // name every button same as its text content
     createBtn.name = buttonText[i-1];
     createBtn.id = 'buttons';
@@ -26,19 +27,57 @@ for (let i=1; i<=19; i++) {
     outmostContainer.appendChild(createBtn);
     createBtn.style.backgroundColor = 'yellow';
     // add event listeners for each button
-    createBtn.addEventListener ('click', displayFunc);
+    if ((operatorsArr.some(value => createBtn.name.includes(value)))) {
+        createBtn.addEventListener ('click', displayFunc);
+    }
      if (createBtn.textContent === '0') {
         createBtn.style.width = '130px';
-    }     
+    } 
+    if((numButtonsArr.some(value => createBtn.name.includes(value)))) {
+        createNumBtn = createBtn;
+        createNumBtn.addEventListener('click', numEventFunc);
+    }   
+    if(createBtn.name === 'AC') {
+        clearButton = createBtn;
+        clearButton.addEventListener('click', clearButtonFunc);
+    }
 }
 
-function add (num1,num2) {
+let currentNum;
+let num1 = '';
+let num2 = '';
+let currentOperator;
+console.log("operator changed to: " +currentOperator);
+
+function add (number1,number2) {
     // screenDiv.textContent = num1 + num2;
-    console.log('add: ' + (num1+ num2));
+    
+    let result = number1 + number2;
+    currentNum = Number(result);
+    console.log('result: ' + result);
+    
+    
+    console.log("add func here num1: " + num1);
+    console.log("add func here num2: " + num2);
+    screenDiv.textContent = result;
+    //num1 = currentNum;
+    num2= '';
+    
+    return result;
 }
 
-function substract (num1,num2) {
-    screenDiv.textContent = num1 - num2;
+function subtract (number1,number2) {
+    let result = number1 - Number(number2);
+    
+    currentNum = Number(result);
+    console.log('result: ' + result);    
+    console.log("add func here num1: " + num1);
+    console.log("add func here num2: " + num2);
+    screenDiv.textContent = result;
+    //num1 = currentNum;
+    num2= '';
+    
+    return result;
 }
 
 function multiply (num1,num2) {
@@ -59,25 +98,52 @@ function changeSign (num1) {
 // deciding which function to use depending on the operator type
 function operate (operator, num1, num2) {
     if(operator === '+') add(num1,num2)
-    if(operator === '-') substract(num1,num2)
+    if(operator === '-') subtract(num1,num2)
     if(operator === 'x') multiply(num1,num2)
     if(operator === '/') divide(num1,num2)
 
 }
  
-let currentNum = '';
-let leftNum = '';
-let currentOperator;
+
 
 
 // select screenDiv
 let screenDiv = document.getElementById('screenDiv');
 
  function displayFunc (e) {
+    let result = 0;
+    currentOperator = e.target.name;
+    e.target.style.backgroundColor = 'white';
 
-    if(!(operatorsArr.some( value => e.target.name.includes(value)))) {
+    
+    
+    if (currentOperator === '+') {
+        add(num1, num2);
+        
+    }
+    
+    if (currentOperator === '-') {
+        subtract(num1, num2);
+        
+    }
+
+    
+    
+    
+    
+    num1 = currentNum;
+    
+    console.log("operator is " + currentOperator);
+    console.log("displayFunc here num1: " + num1);
+    console.log("displayFunc here num2: " + num2);
+
+
+
+
+
+    /* if(!(operatorsArr.some( value => e.target.name.includes(value)))) {
         screenDiv.textContent += e.target.name;
-        leftNum += e.target.name;
+        num1 += e.target.name;
     }
     
 
@@ -88,21 +154,45 @@ let screenDiv = document.getElementById('screenDiv');
         //operator secildiginde ekran bos degilse ve ekranda bir operator varsa:   
         if (screenDiv.textContent !== '' && operatorsArr.some( value => screenDiv.textContent.includes(value))){
     
-        //leftNum = e.target.name;
+        //num1 = e.target.name;
         screenDiv.textContent += e.target.name;
         }
   
 } 
 
 
-console.log(leftNum)
+console.log(num1) */
      
     
 }
         
     
     
+function numEventFunc (e) {
+
+    if(currentOperator == undefined) {
+        screenDiv.textContent += e.target.name;
+        num1 = Number(screenDiv.textContent);
+        
+    } else {
+        if(num2 === '') screenDiv.textContent = '';
+        
+        screenDiv.textContent += e.target.name;
+        num2= Number(screenDiv.textContent);
+    }
+
+
     
+
+   
+   
+   console.log('num1: ' + num1);
+   console.log('num2: ' + num2);
+}
+
+function clearButtonFunc () {
+    console.log("clear button event function called");
+}
     
 
 
