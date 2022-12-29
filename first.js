6d7a21cbf8661ac669250112a926808edb17558c
VycMajoris <ok1907@gmail.com>
2022-12-29 22:31:24 +0300

    add IOS-style appearance and finish the project.

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 let outmostContainer = document.getElementById('outmostContainer');
 
 // text for buttons 
-let buttonText = ["AC", "+/-", "%", "/",
+let buttonText = ["C", "+/-", "%", "/",
                  "7", "8", "9" ,"x", "4",
                 "5", "6", "-", "1","2",
                 "3", "+", "0", ",", "=",];
@@ -17,26 +17,27 @@ for (let i=1; i<=19; i++) {
     let createNumBtn = document.createElement('button');
     let clearButton = document.createElement('button');
     let equalButton = document.createElement('button');
+    let changeSignButton = document.createElement('button');
     // name every button same as its text content
     createBtn.name = buttonText[i-1];
     createBtn.id = 'buttons';
     createBtn.textContent= buttonText[i-1];
-    createBtn.style.width= '65px';
-    createBtn.style.height = '65px';
+    createBtn.style.width= '70px';
+    createBtn.style.height = '70px';
     outmostContainer.appendChild(createBtn);
-    createBtn.style.backgroundColor = 'yellow';
+    
     // add event listeners for each button
     if ((operatorsArr.some(value => createBtn.name.includes(value)))) {
         createBtn.addEventListener ('click', displayFunc);
     }
      if (createBtn.textContent === '0') {
-        createBtn.style.width = '130px';
+        createBtn.style.width = '150px';
     } 
     if((numButtonsArr.some(value => createBtn.name.includes(value)))) {
         createNumBtn = createBtn;
         createNumBtn.addEventListener('click', numEventFunc);
     }   
-    if(createBtn.name === 'AC') {
+    if(createBtn.name === 'C') {
         clearButton = createBtn;
         clearButton.addEventListener('click', clearButtonFunc);
     }
@@ -44,7 +45,17 @@ for (let i=1; i<=19; i++) {
         equalButton = createBtn;
         equalButton.addEventListener('click', equalButtonFunc);
     }
+    if(createBtn.name === '+/-') {
+        changeSignButton = createBtn;
+        changeSignButton.addEventListener('click', changeSign);
+    }
+    
 }
+
+let spaceHolderDiv = document.createElement('div');
+spaceHolderDiv.id = 'spaceHolderDiv';
+outmostContainer.appendChild(spaceHolderDiv);
+
 
 let currentNum;
 let num1 = '';
@@ -57,7 +68,6 @@ function add (number1,number2) {
     
     let result = number1 + number2;
     currentNum = Number(result);
-    console.log('result: ' + result);
     screenDiv.textContent = result;
     num1 = currentNum;
     num2= '';    
@@ -66,7 +76,6 @@ function add (number1,number2) {
 function subtract (number1,number2) {
     let result = number1 - number2;
     currentNum = Number(result);
-    console.log('result: ' + result);
     screenDiv.textContent = result;
     num1 = currentNum;
     num2= '';    
@@ -76,7 +85,6 @@ function multiply (number1,number2) {
     let result = number1 * number2;
     if (number2 === '') multiply (number1,number2);
     currentNum = Number(result);
-    console.log('result: ' + result);
     screenDiv.textContent = result;
     num1 = currentNum;
     num2= '';    
@@ -85,7 +93,6 @@ function multiply (number1,number2) {
 function divide (number1,number2) {
     let result = number1 / number2;
     currentNum = Number(result);
-    console.log('result: ' + result);
     screenDiv.textContent = result;
     num1 = currentNum;
     num2= '';    
@@ -94,45 +101,40 @@ function divide (number1,number2) {
 function percent (number1) {
     let result = number1 / 100;
     currentNum = Number(result);
-    console.log('result: ' + result);
     screenDiv.textContent = result;
     num1 = currentNum;
     num2= '';    
+    
     return result;
 }
- function changeSign (number1) {
-    let result = number1 * -1;
-    currentNum = -1 * Number(result);
-    console.log('result: ' + result);
-    screenDiv.textContent = result;
-    num1 = currentNum;
-    num2= '';    
-    return result;  
+ function changeSign () {
+    //if (num1 === '') screenDiv.textContent = '';
+    /* if (num1>0) screenDiv.textContent = -num1;
+    else if (num1<0) screenDiv.textContent = num1;
+    else screenDiv.textContent = screenDiv.textContent; */
+    num1 = -num1;
+    console.log(num1);
+    screenDiv.textContent = num1;
+    
+    
 } 
 
-
-// deciding which function to use depending on the operator type
-function operate (operator, num1, num2) {
-    if(operator === '+') add(num1,num2)
-    if(operator === '-') subtract(num1,num2)
-    if(operator === 'x') multiply(num1,num2)
-    if(operator === '/') divide(num1,num2)
-
-}
 
 // select screenDiv
 let screenDiv = document.getElementById('screenDiv');
 
  function displayFunc (e) {
 
-    e.target.style.backgroundColor = 'white';    
+    
+    //e.target.style.backgroundColor = 'white';    
     if (currentOperator === '+') add(num1, num2);    
     if (currentOperator === '-') subtract(num1, num2);
     if (currentOperator === 'x') multiply(num1, num2);
     if (currentOperator === '/') divide(num1, num2);
     if (currentOperator === '%') percent(num1);
-    if (currentOperator === '+/-') changeSign(num1);
-    if (currentOperator === 'AC') changeSign(num1);
+    if (currentOperator === '+/-') changeSign();
+    if (currentOperator === 'C') clearButtonFunc();
+    
     currentOperator = e.target.name;
 }
 
@@ -148,10 +150,8 @@ function numEventFunc (e) {
     }
 }
 function clearButtonFunc () {
-    num1 = '';
-    num2 = '';
-    currentOperator = '';
-    screenDiv.textContent = '';
+
+    location.reload();
 }
 
 function equalButtonFunc () {
@@ -159,10 +159,18 @@ function equalButtonFunc () {
 }
     
 
-// TODO: Pressing an operator twice makes result 0.
 
 
-
+function applyStyles() {
+    // styles go here
+    var buttons = document.querySelectorAll("#buttons > *:nth-child(-n+3)");
+    buttons.forEach(function(button) {
+      button.style.width = "50px";
+      button.style.height = "50px";
+      button.style.borderRadius = "25px";
+    });
+  }
+  
    
         
         
